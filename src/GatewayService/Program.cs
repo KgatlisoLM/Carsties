7699f46545f713bb40e4532bf637432dffb5e8a3
Kgatliso Matema <kgatliso481@live.com>
2024-03-02 16:51:38 +0200

    Section 11

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -7,7 +7,7 @@ builder.Services.AddReverseProxy()
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
   .AddJwtBearer(options => {
-    options.Authority = builder.Configuration["IdentityServerUrl"];
+    options.Authority = builder.Configuration["IdentityServiceUrl"];
     options.RequireHttpsMetadata = false;
     options.TokenValidationParameters.ValidateAudience = false;
     options.TokenValidationParameters.NameClaimType = "username";
